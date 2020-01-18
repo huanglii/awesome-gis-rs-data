6d718424d44626f45b6517006b984c615338223b
huangli <849151701@qq.com>
2020-01-18 12:34:59 +0800

    M: 访问量

--- src/components/FooterBar/index.jsx
+++ src/components/FooterBar/index.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Layout, Tooltip, Icon, Divider } from 'antd'
+import { Layout } from 'antd'
 
 import './index.less'
 
@@ -11,13 +11,7 @@ class FooterBar extends Component {
       <Footer className='footer-bar'>
         <span>© 2020 by <a href='https://github.com/huanglii' rel='noopener noreferrer' target='_blank'>huangli</a></span>
         <br />
-        <Tooltip placement='top' title='访问数'>
-          <span><Icon type='user' /><span id='busuanzi_value_site_pv' /></span>
-        </Tooltip>
-        <Divider type='vertical' />
-        <Tooltip placement='top' title='访问量'>
-          <span><Icon type='eye' /><span id='busuanzi_value_site_uv' /></span>
-        </Tooltip>
+        <span>总访问量：<span id='busuanzi_value_page_pv' /> 次</span>
       </Footer>
     )
   }
