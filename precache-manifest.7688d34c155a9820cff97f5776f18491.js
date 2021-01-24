e5414f2fe4e8c2f3ce00d56a6503aa326c812662
黄俐 <849151701@QQ.COM>
2021-01-24 20:56:07 +0800

    Updates

---
+++ precache-manifest.7688d34c155a9820cff97f5776f18491.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "574618012049766a4b4832bc89798330",
+    "url": "/awesome-gis-rs-data/index.html"
+  },
+  {
+    "revision": "2ae51f6044abd05a1eb5",
+    "url": "/awesome-gis-rs-data/static/css/2.759a6b9d.chunk.css"
+  },
+  {
+    "revision": "ddc15c64520bf7fef1b7",
+    "url": "/awesome-gis-rs-data/static/css/main.ef63870c.chunk.css"
+  },
+  {
+    "revision": "2ae51f6044abd05a1eb5",
+    "url": "/awesome-gis-rs-data/static/js/2.337cd8e5.chunk.js"
+  },
+  {
+    "revision": "3e27df191eee64ca21c3c05d6eea271f",
+    "url": "/awesome-gis-rs-data/static/js/2.337cd8e5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ddc15c64520bf7fef1b7",
+    "url": "/awesome-gis-rs-data/static/js/main.fd7a4e93.chunk.js"
+  },
+  {
+    "revision": "e5fe4bda90ba9b3bdbc3",
+    "url": "/awesome-gis-rs-data/static/js/runtime-main.87a003d0.js"
+  },
+  {
+    "revision": "3b513c42d53a2db6f60fc27ffab3521d",
+    "url": "/awesome-gis-rs-data/static/media/bg.3b513c42.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.03f339663c5858f1863d9973c538ffab.js"
+  "/awesome-gis-rs-data/precache-manifest.7688d34c155a9820cff97f5776f18491.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/awesome-gis-rs-data/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
